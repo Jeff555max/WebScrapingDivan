7dd41aa36d225af4257b84ca467885f7f4dbb4e5
Jeff555max <vadik98765@gmail.com>
2024-12-07 15:55:55 +0500

    add code

---
+++ divan_scraper/divan_scraper/spiders/divan.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class DivanSpider(scrapy.Spider):
+    name = "divan"
+    allowed_domains = ["divan.ru"]
+    start_urls = [
+        "https://www.divan.ru/chelyabinsk/search?ProductSearch%5Bname%5D=источник+освещения&no_cache=1"
+    ]
+
+    def parse(self, response):
+        # Извлечение карточек товаров
+        products = response.xpath('//div[contains(@class, "_Ud0k")]')
+
+        for product in products:
+            yield {
+                'name': product.xpath('.//a[contains(@class, "ProductName")]/span/text()').get(),
+                'price': product.xpath('.//span[contains(@class, "price")]/text()').get(),
+                'link': response.urljoin(product.xpath('.//a[contains(@class, "ProductName")]/@href').get()),
+            }
+
+        # Переход на следующую страницу (пагинация)
+        next_page = response.xpath('//a[contains(@class, "pagination-next")]/@href').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
